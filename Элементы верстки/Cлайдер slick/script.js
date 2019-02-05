0ce3e4b2352efbeab97a66e04b688017c0a0e8f0
Nikita Kirillov <nik2010spartak@yandex.ru>
2019-02-05 18:10:00 +0300

    Простой слайдер slick, плагин не в репозитории, подключается отдельно

---
+++ Элементы верстки/Cлайдер slick/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        adaptiveHeight: true
+    });
+});
